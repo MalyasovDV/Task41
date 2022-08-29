2357edabdfed1829464d883ec7c4a241c69c9390
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-08-29 14:58:32 +0800

    Task done

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int CountPositiveNumbers(int m)
+{
+    int a, count = 0;
+
+    for (int i = 0; i < m; ++i)
+    {
+        a = int.Parse(Console.ReadLine());
+        if (a > 0)
+            ++count;
+    }
+
+    return count;
+}
+
+int m;
+m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\n" + CountPositiveNumbers(m));
